6ea8951e8167fe551243da4e272876da7e2c1764
Terence2018 <ting704@gmail.com>
2021-12-27 16:17:02 -0800

    Added Coordinate Checks and Wall Creation

--- Board.java
+++ Board.java
@@ -13,6 +13,7 @@ public class Board {
     private int col;
     private int row;
     private char[][] board;
+    private int occupancy;
     
     public Board (int colInput, int rowInput) {
 
@@ -34,6 +35,10 @@ public class Board {
                 }
             }
         }
+        
+        // Occupancy of the gameboard is how many walls can be drawn
+        this.occupancy = (colInput * (rowInput + 1)) + 
+            (rowInput * (colInput + 1));
     }
 
     public void clearBoard () {
@@ -49,6 +54,10 @@ public class Board {
                 }
             }
         }
+    }
+
+    public int getOccupancy () {
+        return occupancy;
     }
 
     public String toString () {
@@ -85,6 +94,105 @@ public class Board {
 
         }
         return returned;
+    }
+
+    public boolean testPosition (String input) {
+
+        // Gets the Character from the string
+        char inputChar = input.charAt(0);
+        // Gets the int from the string
+        int inputInt;
+        try {
+            inputInt = Integer.parseInt(input.substring(1));
+        }
+        catch (Exception e) {
+            System.out.print("Invalid Format: ");
+            return false;
+        }
+
+        // Converts the input to array indexes
+        int colNum = inputChar - (int)'A';
+        int rowNum = inputInt - 1;
+
+        // Check if the indexes are within the table confines
+        if ((colNum >= col) || (colNum < 0)) {
+            System.out.print("Column " + inputChar + ": ");
+            return false;
+        }
+        if ((rowNum >= row) || (rowNum < 0)) {
+            System.out.print("Row " + inputInt + ": ");
+            return false;
+        }
+
+        // If on an even column number
+        if (colNum % 2 == 0) {
+            // If row is even, position not possible
+            if (rowNum % 2 == 0) {
+                System.out.print("Invalid Position: ");
+                return false;
+            }
+            // If row is odd
+            else {
+                // return true if empty
+                if (board[rowNum][colNum] == ' ') {
+                    return true;
+                }
+                // position is full
+                else {
+                    System.out.print("Position Filled: ");
+                    return false;
+                }
+            }
+        }
+        // If on an odd column number
+        else {
+            // If row is even
+            if (rowNum % 2 == 0) {
+                // return true if empty
+                if (board[rowNum][colNum] == ' ') {
+                    return true;
+                }
+                // position is full
+                else {
+                    System.out.print("Position Filled: ");
+                    return false;
+                }
+            }
+            // If row is odd, position not possible
+            else {
+                System.out.print("Invalid Position: ");
+                return false;
+            }
+        }
+    }
+
+    public void fillPosition (String input) {
+  
+        // Check if position is available
+        if (testPosition(input) && (occupancy > 0)) {
+            // Gets input from String
+            char inputChar = input.charAt(0);
+            int inputInt = Integer.parseInt(input.substring(1));
+            // Converts the input to array indexes
+            int colIndex = inputChar - (int)'A';
+            int rowIndex = inputInt - 1;
+
+            // If colIndex is even, then fill with vertical wall
+            if (colIndex % 2 == 0) {
+                board[rowIndex][colIndex] = (char)9553;
+                occupancy--;
+            }
+            // If colIndex is odd, then fill with horizontal wall
+            else {
+                board[rowIndex][colIndex] = (char)9552;
+                occupancy--;
+            }
+
+        }
+        // Position is not available
+        else {
+            System.out.println("Not Available");
+        }
     }
 
 }
--- Driver.java
+++ Driver.java
@@ -21,21 +21,46 @@ public class Driver {
         System.out.println("██║  ██║██║   ██║   ██║   ╚════██║    ██╔══██║██║╚██╗██║██║  ██║    ██╔══██╗██║   ██║ ██╔██╗ ██╔══╝  ╚════██║");
         System.out.println("██████╔╝╚██████╔╝   ██║   ███████║    ██║  ██║██║ ╚████║██████╔╝    ██████╔╝╚██████╔╝██╔╝ ██╗███████╗███████║");
         System.out.println("╚═════╝  ╚═════╝    ╚═╝   ╚══════╝    ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝     ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝\n");
+        // Subtitles 
         System.out.println("By Terence Ting");
-        System.out.println("Font courtesy of Patorjk.com\n");
+        System.out.println("Font courtesy of Patorjk.com");
+        System.out.println("Ctrl+D at any point to manually end the game\n");
 
         Scanner userInput = new Scanner(System.in);
         int columns = 0;
         int rows = 0;
 
         while (true) {
+            // Prompts user for number of columns
+            System.out.print("Number of columns: ");
             try {
-                // Prompts user for number of columns
-                System.out.print("Number of columns: ");
-                columns = userInput.nextInt();
-
-                break;
+                if (userInput.hasNextLine()) {
+                    columns = userInput.nextInt();
+                }
+                // EOF detected
+                else {
+                    System.out.println("EOF Detected");
+                    System.out.println("\nGame Completed\n");
+                    System.exit(0);
+                }
+                
+                // Check if column number is smaller than 1
+                if (columns < 1) {
+                    System.out.println("Must Have at Least 1 Column");
+                    userInput = new Scanner(System.in);
+                    continue;
+                }
+                // Check if column number is greater than 10
+                else if (columns >= 10) {
+                    System.out.println("You might be here for a while with " + columns + " Columns (Maximum 9 Columns)");
+                    userInput = new Scanner(System.in);
+                    continue;
+                }
+                else {
+                    break;
+                }
             }
+            // Catch any non integer input
             catch (Exception e) {
                 System.out.println("ERROR: Not Integer Input");
                 userInput = new Scanner(System.in);
@@ -43,27 +68,88 @@ public class Driver {
 
         }
 
+        // Separator line
+        System.out.println();
         userInput = new Scanner(System.in);
 
         while (true) {
+            // Prompts user for number of rows
+            System.out.print("Number of rows: ");
             try {
-                // Prompts user for number of rows
-                System.out.print("Number of rows: ");
+                if (userInput.hasNextLine()) {
                 rows = userInput.nextInt();
-
-                break;
+                }
+                // Checks for EOF
+                else {
+                    System.out.println("EOF Detected");
+                    System.out.println("\nGame Completed\n");
+                    System.exit(0);
+                }
+            
+                // checks if row number is smaller than 1
+                if (rows < 1) {
+                    System.out.println("Must Have at Least 1 Row");
+                    userInput = new Scanner(System.in);
+                    continue;
+                }
+                // Checks if row number is greater than 10
+                else if (rows >= 10) {
+                    System.out.println("You might be here for a while with " + rows + " Rows (Maximum 9 Rows)");
+                    userInput = new Scanner(System.in);
+                    continue;
+                }
+                else {
+                    break;
+                }
             }
+            // Catches non integer input
             catch (Exception e) {
                 System.out.println("ERROR: Not Integer Input");
                 userInput = new Scanner(System.in);
             }
         }
 
+        // creates the gameboard
         Board gameBoard = new Board(columns, rows);
 
+        // Prints the empty gameboard
         System.out.println("\n" + gameBoard.toString());
 
-        System.out.println("\n" + gameBoard.toString());
+        // Loop to add walls to the board
+        while (true) {
+            System.out.print("Fill a position: ");
+
+            userInput = new Scanner(System.in);
+            String position;
+
+            // Checks for EOF
+            if (userInput.hasNextLine()) {
+                position = userInput.nextLine();
+            }
+            else {
+                System.out.println("EOF Detected");
+                break;
+            }
+
+            // Fills position if available
+            if (gameBoard.testPosition(position)) {
+                gameBoard.fillPosition(position);
+                // Only prints the board if changes have been made
+                System.out.println(gameBoard.toString());
+            }
+            else {
+                System.out.println("Not Avaiable");
+            }
+
+            // If game board no longer has any spaces left, end
+            if (gameBoard.getOccupancy() == 0) {
+                break;
+            }
+        }
+
+        // Game completed message
+        System.out.println("\nGame Completed\n");
+
 
     }
 
